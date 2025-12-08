29be82696a9dd21e4371d6fdaf0ed652519f2906
enswapwar <115549512+enswapwar@users.noreply.github.com>
2025-12-08 23:39:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
+
+$context = stream_context_create([
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)\r\n"
+    ]
+]);
+
 $url = "https://www1.x-feeder.info/lo18bx2n/rss.php";
-$rss = simplexml_load_file($url);
+
+$rss = @simplexml_load_file($url, null, LIBXML_NOCDATA, "", true, $context);
 
 if (!$rss) {
-    echo "RSS取得失敗";
+    echo "RSS読み込み失敗（UA偽装も無理）\n";
     exit;
 }
 
-$latest = $rss->channel->item[0];
-
-$title = (string)$latest->title;     // "(123) 名前" 的なやつ
-$desc  = (string)$latest->description;
-
-echo "最新コメント\n";
-echo "TITLE: $title\n";
-echo "DESC: $desc\n";
+print_r($rss);
